fa5aa214e168420caf6ab82ddeb5c5bfc3648965
GitHub CI <xjossy@yandex-team.ru>
2024-04-13 10:53:23 +0000

    Обновление репы из коммита f6670e4f73d55e20bae3db738089c32fd1979213

---
+++ problems/s02/close-button/submission/close_button.pro
@@ -0,0 +1,13 @@
+QT       += core gui widgets
+
+CONFIG += c++20
+
+SOURCES += \
+    main.cpp \
+    mainwindow.cpp
+
+HEADERS += \
+    mainwindow.h
+
+FORMS += \
+    mainwindow.ui
---
+++ problems/s02/close-button/tests/tests.cpp
@@ -0,0 +1,86 @@
+#include <QtTest>
+
+#include <QtTest>
+#include <QCoreApplication>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QGridLayout>
+#include <QtWidgets/QLineEdit>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QToolButton>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QWidget>
+
+#include <mainwindow.h>
+
+// add necessary includes here
+
+class TestYourApp : public QObject
+{
+    Q_OBJECT
+
+    template<class T>
+    T* findElementByText(const QString& label) {
+        for (T* button : window->findChildren<T*>())
+        {
+            if (button->text() == label)
+            {
+                return button;
+            }
+        }
+        return nullptr;
+    }
+
+public:
+    TestYourApp()=default;
+    ~TestYourApp()=default;
+
+private slots:
+    void initTestCase();
+    void cleanupTestCase();
+    void TestCloseButton();
+
+private:
+    MainWindow* window;
+    QPushButton *yourButton;
+    QLineEdit *textView;
+    QApplication *app;
+};
+
+
+void TestYourApp::initTestCase()
+{
+    int argc = 0;
+    char* argv[] = {};
+    app = new QApplication(argc, argv);
+
+    window = new MainWindow();
+
+    QVERIFY2(window != nullptr, "Окно приложения не создано");
+    window->show();
+    QVERIFY2(window->isVisible(), "Окно приложения не активируется");
+
+
+    auto buttons = window->findChildren<QPushButton*>();
+    QVERIFY2(buttons.size() == 1, "В приложении должна быть ровно одна копка QPushButton");
+    yourButton = buttons.first();
+    
+}
+
+void TestYourApp::TestCloseButton()
+{
+
+    QTest::mouseClick(yourButton, Qt::LeftButton);
+    QVERIFY2(!window->isVisible(), "Окно приложения не было закрыто после нажатия на кнопку");
+
+}
+
+void TestYourApp::cleanupTestCase()
+{
+    delete app;
+}
+
+QTEST_APPLESS_MAIN(TestYourApp)
+
+#include "tests.moc"
