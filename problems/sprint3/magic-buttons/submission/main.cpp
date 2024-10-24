1dfc266aa7df1d201f889c379c66b60b8d110323
GitHub CI <xjossy@yandex-team.ru>
2024-10-24 09:22:50 +0000

    Обновление репы из коммита 53668ea9ce6dd95fb405d3721acc88e641f7360c

--- problems/sprint3/magic-buttons/submission/main.cpp
+++ problems/sprint3/magic-buttons/submission/main.cpp
@@ -1,11 +1,11 @@
-#include "selector.h"
+#include "mainwindow.h"
+
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    Selector w;
+    MainWindow w;
     w.show();
-
     return a.exec();
 }
